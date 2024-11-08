d841819bfec9527bf1817ac00aba5c8a5bf82022
Mathieu Rossi <mathieurossi77@gmail.com>
2024-11-08 16:48:40 +0100

    fix: scroll

--- src/lib/utils/update-position.ts
+++ src/lib/utils/update-position.ts
@@ -1,5 +1,5 @@
 export function updatePosition(targetEl: HTMLElement, clickX: number, clickY: number) {
 	const rect = targetEl.getBoundingClientRect();
 
-	return { x: rect.left + clickX, y: rect.top + clickY };
+	return { x: rect.left + clickX + window.scrollX, y: rect.top + clickY + window.scrollY };
 }
